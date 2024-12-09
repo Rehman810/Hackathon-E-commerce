f504d7d85389634ae6e74ee5e715eb12c0f185cd
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-12-09 21:39:42 +0500

    pricing page done

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Pricebanner from '../components/PriceBanner/Pricebanner'
+import PricingFaq from '../components/PricingFaq/PricingFaq'
+import CtaBanner from '../components/Cta/CtaBanner'
 
 const page = () => {
   return (
     <div>
       <Pricebanner/>
+      <PricingFaq/>
+      <CtaBanner/>
     </div>
   )
 }
